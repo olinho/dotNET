334a1645a02aeb5f421e2508c59162cbe979492c
olinho <olinho1994@wp.pl>
2016-04-29 14:05:03 +0200

    Lesson 9 sollution

---
+++ Lesson_9/ClassLibrary1/WebApplication1/App_Start/WebApiConfig.cs
@@ -0,0 +1,39 @@
+﻿using System.Web.Http;
+using LibraryGames;
+using System.Web.OData.Extensions;
+using System.Web.OData.Builder;
+
+namespace WebApplication1
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            //config.SuppressDefaultHostAuthentication();
+            //config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            //// Web API routes
+            //config.MapHttpAttributeRoutes();
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+
+            builder.EntitySet<Game>("Games");
+            builder.EntitySet<Store>("Stores");
+
+            builder.Function("Papa").Returns<double>().Parameter<int>("Postal");
+
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: null,
+                model: builder.GetEdmModel());
+        }
+    }
+}
---
+++ Lesson_9/LibraryGames/GamesContext.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryGames
+{
+    public class GamesContext : DbContext
+    {
+        public GamesContext() : base("GamesContext2")
+        {
+
+        }
+
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Store> Stores { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("public");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
